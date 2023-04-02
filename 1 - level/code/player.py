3e04a11c3294f85ec241e030c0b8426200cafce5
Edwin J. Calderon <calderonedwin3@gmail.com>
2023-04-01 22:15:47 -0500

    Updating player file

--- 1 - level/code/player.py
+++ 1 - level/code/player.py
@@ -8,3 +8,22 @@ class Player(pygame.sprite.Sprite):															# 1 inherit sprite.Sprite from
 		# 1 self.image and rect are the two things you need for any sprite
 		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()		# 1 convert_alpha smooths the png's edges
 		self.rect = self.image.get_rect(topleft = pos)										# 1 will give the tile a position of topleft
+
+		self.direction = pygame.math.Vector2()												# 1 player direction movement
+
+	def input(self):																		# 1 this is how you get keyboard input for up down left and right
+		keys = pygame.key.get_pressed()														# 1 to get keyboard inputs
+
+		if keys[pygame.K_UP]:																# 1 checks if the key is being pressed up
+			self.direction.y = -1
+		elif keys[pygame.K_DOWN]:															# 1 checks if the key is being pressed down
+			self.direction.y = 1
+		else:																				# 1 if nothing is being pressed then no direction changes
+			self.direction.y = 0
+
+		if keys[pygame.K_RIGHT]:															# 1 checks if the key is being pressed right
+			self.direction.x = 1
+		elif keys[pygame.K_LEFT]:															# 1 checks if the key is being pressed left
+			self.direction.x = -1
+		else:																				# 1 if nothing is being pressed then no direction changes
+			self.direction.x = 0
